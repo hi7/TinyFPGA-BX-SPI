89d634b5f67a4dcca53dabdc870d9128904d5632
Hilger Baumstark <hilger.baumstark@hamburg.de>
2021-01-04 18:00:13 +0100

    expands by data/command and backlight

--- top.v
+++ top.v
@@ -1,43 +1,46 @@
 // look in pins.pcf for all the pin names on the TinyFPGA BX board
 module top (
-    output CSX, // chip select, active low
-    output SDA, // serial data
-    output SCL, // serial clock max period 66ns
-    output RESX, // reset, active low
-    // User/boot LED next to power LED
+    output CSX,   // chip select, active low
+    output SDA,   // serial data
+    output SCL,   // serial clock max period 66ns
+    output RESX,  // reset, active low
+    output DCX,   // 1 => data / 0 => command
+    output BL,    // backlight
     output USBPU, // USB pull-up resistor
     input CLK     // 16MHz clock, period 62.5 ns
 );
-    localparam  LEAD_IN = 6;
-    // drive USB pull-up resistor to '0' to disable USB
-    assign USBPU = 0;
+    localparam LEAD_IN = 1000;
+    localparam LENGTH = 18;
+    localparam MSB = LENGTH - 1;
+    reg init = 1;
+    reg USBPU = 0; // drive USB pull-up resistor to '0' to disable USB
     reg RESX = 1;
-    reg CSX = 1;
+    reg DCX = 1;
+    reg BL = 0;
     reg SCL = 0;
     reg SDA = 0;
-    reg [5:0] counter = 0;
-    reg [4:0] index = 0;
-    reg [17:0] rgb = 18'b100000111000110001;
+    reg [11:0] counter = 0;
+    reg [10:0] index = 0;
+    reg [MSB:0] rgb = 18'b100000111000110001;
+    reg CSX = 1;
 
-    // increment the blink_counter every clock
-    always @(posedge CLK) begin
-      if(CSX == 0) SCL <= ~SCL;
-      if(counter == 2) RESX <= 0;
-      if(counter == LEAD_IN-2) RESX <= 1;
-      if(counter == LEAD_IN) CSX <= 0;
-      if(index > 17) CSX <= 1;
+    always @(negedge CLK) begin
+      CSX <= counter < LEAD_IN || counter > (LEAD_IN + MSB*2 + 1);
       if(counter >= LEAD_IN) begin
-        index = counter[5:1] - 3;
+        index = counter[11:1] - (LEAD_IN / 2);
         if(counter % 2 == 0) SDA <= rgb[index];
       end
-      if(index > 17) begin
-        SCL <= 0;
-        SDA <= 0;
+      if(counter > LEAD_IN + MSB*2 + 1) SDA <= 0;
+    end
+
+    always @(posedge CLK) begin
+      if(CSX == 0 && counter <= (LEAD_IN + MSB*2 + 2)) SCL <= ~SCL;
+      if(init == 1 && counter == 160) RESX <= 0;
+      if(init == 1 && counter == 162) RESX <= 1;
+      if(counter == LEAD_IN) begin
+        init <= 0;
+        BL <= 1;
       end
       counter <= counter + 1;
     end
-
-    // light up the LED according to the pattern
-    //assign LED = blink_pattern[blink_counter[25:21]];
-    //assign LED = clk_8;//blink_counter[2];
 endmodule
--- top_tb.v
+++ top_tb.v
@@ -4,14 +4,22 @@
 
 module top_tb();
 
-parameter DURATION = 2772; // 42 * 66ns
+parameter DURATION = 528000; // 8000 * 66ns
 
 reg clk_16=1;
 always #31.25 clk_16 = ~clk_16; // 16MHz
 
-wire resx, csx, sda, scl, usbpu;
+wire resx, dcx, bl, csx, sda, scl, usbpu;
 
-top UUT(.RESX(resx), .CSX(csx), .SDA(sda), .SCL(scl), .USBPU(usbpu), .CLK(clk_16));
+top UUT(
+  .RESX(resx),
+  .DCX(dcx),
+  .BL(bl),
+  .CSX(csx),
+  .SDA(sda),
+  .SCL(scl),
+  .USBPU(usbpu),
+  .CLK(clk_16));
 
 initial begin
     $dumpfile("top_tb.vcd");
